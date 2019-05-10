b76bc9de8077f343d210a2d6216d87dc28e19957
AD <di-marco_a@pm.me>
2019-05-10 18:23:19 +0200

    add var of path

--- private/.sync.sh
+++ private/.sync.sh
@@ -19,7 +19,7 @@ HOST_TARGET='192.168.42.3' # hHst to deploy inside port is 192.168.42.3 @ outsid
 PATH_TARGET='~/r2d2/it/homekit/homebridge-macosx-info' # Path of script directory 
 NODE_MODULE_TARGET='/usr/local/lib/node_modules/homebridge-macosx-info'
 
-PORT_TARGET=22 # ssh port (inside port is 22 @ outside port is 10022) 
+PORT_TARGET=22 # ssh port (inside port is 22) 
 
 PLIST_TARGET='~/Library/LaunchAgents/' # Directory for plist file 
 CONFIG_HOMEBRIDGE_TARGET='~/.homebridge/'
@@ -46,17 +46,25 @@ chmod +x $SRC/src/sh/*.sh
 printf "$red" "-> save in $PATH_TARGET ..."
 /usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "mkdir -p ${PATH_TARGET}/src/sh"
 /usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "mkdir -p ${PATH_TARGET}/docs"
+/usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "mkdir -p ${PATH_TARGET}/screenshots"
+
 scp -P $PORT_TARGET -rp $SRC/LICENSE $USER_TARGET@$HOST_TARGET:$PATH_TARGET/LICENSE
 scp -P $PORT_TARGET -rp $SRC/package.json $USER_TARGET@$HOST_TARGET:$PATH_TARGET/package.json
 scp -P $PORT_TARGET -rp $SRC/README.md $USER_TARGET@$HOST_TARGET:$PATH_TARGET/README.md
 scp -P $PORT_TARGET -rp $SRC/index.js $USER_TARGET@$HOST_TARGET:$PATH_TARGET/index.js
 scp -P $PORT_TARGET -rp $SRC/src/sh/homebridge-macosx-info.sh $USER_TARGET@$HOST_TARGET:$PATH_TARGET/src/sh/homebridge-macosx-info.sh
 scp -P $PORT_TARGET -rp $SRC/docs/CHANGELOG.md $USER_TARGET@$HOST_TARGET:$PATH_TARGET/docs/CHANGELOG.md
+scp -P $PORT_TARGET -rp $SRC/screenshots/.fake.png $USER_TARGET@$HOST_TARGET:$PATH_TARGET/screenshots/.fake.png
+scp -P $PORT_TARGET -rp $SRC/screenshots/scrennshot_1.png $USER_TARGET@$HOST_TARGET:$PATH_TARGET/screenshots/scrennshot_1.png
+scp -P $PORT_TARGET -rp $SRC/screenshots/scrennshot_2.png $USER_TARGET@$HOST_TARGET:$PATH_TARGET/screenshots/scrennshot_2.png
+scp -P $PORT_TARGET -rp $SRC/screenshots/wallpaper.png $USER_TARGET@$HOST_TARGET:$PATH_TARGET/screenshots/wallpaper.png
 
 
 printf "$red" "-> Deploy in $NODE_MODULE_TARGET ..."
 /usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "sudo mkdir -p ${NODE_MODULE_TARGET}/src/sh"
 /usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "sudo mkdir -p ${NODE_MODULE_TARGET}/docs"
+/usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "sudo mkdir -p ${NODE_MODULE_TARGET}/screenshots"
+
 /usr/bin/ssh -p $PORT_TARGET $USER_TARGET@$HOST_TARGET "sudo chown -R AD:staff ${NODE_MODULE_TARGET}"
 
 scp -P $PORT_TARGET -rp $SRC/LICENSE $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/LICENSE
@@ -65,6 +73,10 @@ scp -P $PORT_TARGET -rp $SRC/package.json $USER_TARGET@$HOST_TARGET:$NODE_MODULE
 scp -P $PORT_TARGET -rp $SRC/index.js $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/index.js
 scp -P $PORT_TARGET -rp $SRC/src/sh/homebridge-macosx-info.sh $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/src/sh/homebridge-macosx-info.sh
 scp -P $PORT_TARGET -rp $SRC/docs/CHANGELOG.md $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/docs/CHANGELOG.md
+scp -P $PORT_TARGET -rp $SRC/screenshots/.fake.png $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/screenshots/.fake.png
+scp -P $PORT_TARGET -rp $SRC/screenshots/scrennshot_1.png $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/screenshots/scrennshot_1.png
+scp -P $PORT_TARGET -rp $SRC/screenshots/scrennshot_2.png $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/screenshots/scrennshot_2.png
+scp -P $PORT_TARGET -rp $SRC/screenshots/wallpaper.png $USER_TARGET@$HOST_TARGET:$NODE_MODULE_TARGET/screenshots/wallpaper.png
 
 
 printf "$red" "  |_ homebridge restart"
